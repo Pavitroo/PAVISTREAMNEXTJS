445db0dc5e150079fe68a56cd9d6ab66db534175
Pavitroo <pavitraguptadelhi@gmail.com>
2026-01-07 21:47:15 +0530

    Fix: install react-router-dom and types

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,6 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { HelmetProvider } from "react-helmet-async";
 import { ThemeProvider } from "@/hooks/useTheme";
 import { SupabaseAuthProvider } from "@/contexts/SupabaseAuthContext";
 import ProtectedRoute from "@/components/ProtectedRoute";
@@ -26,38 +24,17 @@ import TermsPage from "./pages/TermsPage";
 const queryClient = new QueryClient();
 
 const App = () => (
-  <HelmetProvider>
-    <ThemeProvider>
-      <QueryClientProvider client={queryClient}>
-        <SupabaseAuthProvider>
-          <TooltipProvider>
-            <Toaster />
-            <Sonner />
-            <BrowserRouter>
-              <Routes>
-                <Route path="/" element={<LandingPage />} />
-                <Route path="/auth" element={<AuthPage />} />
-                <Route path="/home" element={<ProtectedRoute><HomePage /></ProtectedRoute>} />
-                <Route path="/profile" element={<ProtectedRoute><ProfilePage /></ProtectedRoute>} />
-                <Route path="/movies" element={<ProtectedRoute><MoviesPage /></ProtectedRoute>} />
-                <Route path="/movies/:id" element={<ProtectedRoute><MovieDetailPage /></ProtectedRoute>} />
-                <Route path="/shows" element={<ProtectedRoute><ShowsPage /></ProtectedRoute>} />
-                <Route path="/shows/:id" element={<ProtectedRoute><ShowDetailPage /></ProtectedRoute>} />
-                <Route path="/search" element={<ProtectedRoute><SearchPage /></ProtectedRoute>} />
-                <Route path="/ai" element={<ProtectedRoute><AIPage /></ProtectedRoute>} />
-                <Route path="/about" element={<AboutPage />} />
-                <Route path="/contact" element={<ContactPage />} />
-                <Route path="/privacy" element={<PrivacyPage />} />
-                <Route path="/terms" element={<TermsPage />} />
-                {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-                <Route path="*" element={<NotFound />} />
-              </Routes>
-            </BrowserRouter>
-          </TooltipProvider>
-        </SupabaseAuthProvider>
-      </QueryClientProvider>
-    </ThemeProvider>
-  </HelmetProvider>
+  <ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <SupabaseAuthProvider>
+        <TooltipProvider>
+          <Toaster />
+          <Sonner />
+          {/* Page content will be rendered by Next.js routing. */}
+        </TooltipProvider>
+      </SupabaseAuthProvider>
+    </QueryClientProvider>
+  </ThemeProvider>
 );
 
 export default App;
